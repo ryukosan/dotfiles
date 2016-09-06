5367c22b3b88d1e109cced689be9c84748c0a16f
ryukosan <ryuko@dessart.co.jp>
2016-09-06 21:43:12 +0900

    Update header.php

--- wp-content/themes/manko_pc/header.php
+++ wp-content/themes/manko_pc/header.php
@@ -1,1 +1,27 @@
-echo $SHELL unko
+<!doctype html>
+<html class="no-js" lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>タイトル</title>
+        <meta name="description" content="説明文">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- アイコンのファイルはルートディレクトに配置 -->
+ 
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 9]>
+            <p class="browserupgrade">IE9未満用のメッセージ</p>
+        <![endif]-->
+ 
+        <!-- ここにコンテンツ -->
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
